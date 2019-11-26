c22addf98135c0dfd80703711d8e82f7e24b6358
wangjin <13572494149@163.com>
2019-11-26 14:53:14 +0800

    angular_cli

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+//引入修饰器
+import {Component} from  '@angular/core'
+//配置组件
+@component({
+  selector:'app-server',
+  templateUrl:'./server.component.html'
+})
+//定义组件类
+export class ServerComponent{
+
+}
